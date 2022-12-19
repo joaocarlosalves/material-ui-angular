20ba51269d6b80a549689b1f79275a775b1b3cd8
unknown <jca1983@gmail.com>
2022-12-19 17:22:04 -0300

    '13r132r12r1r2wqwfq'

--- api/json-server/index.js
+++
@@ -1,9 +0,0 @@
-const users = require('./users.json');
-const posts = require('./posts.json');
-const people = require('./people.json');
-
-module.exports = () => ({
-  users: users,
-  posts: posts,
-  people: people
-});
